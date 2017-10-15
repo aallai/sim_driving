791aefd7dddb65e6c03dffe284fe7b82e1dd377d
Alexandre Allaire <alexandre.allaire@mail.mcgill.ca>
2017-10-15 17:59:24 +0000

    Add image cropiing and normalization.

--- model.py
+++ model.py
@@ -1,10 +1,11 @@
 import keras
-from keras.layers import Flatten, Dense, Activation 
+from keras.layers import Flatten, Dense, Activation, Lambda, Convolution2D 
 import cv2
 import csv
 import numpy as np
 
 DATA_PATH = '/home/carnd/data'
+IMAGE_SHAPE =(160, 320, 3)
 
 # TODO: use other features.
 def load_data():
@@ -23,7 +24,11 @@ def load_data():
 # TODO: improve architecture.
 def network(x):
     net = keras.models.Sequential()
-    net.add(Flatten(input_shape=x.shape[1:]))
+
+    # Crop off top third of image.
+    net.add(Lambda(lambda x: x[:,50:,...], input_shape=IMAGE_SHAPE))
+    net.add(Lambda(lambda x: (x / 255.0) - 0.5))
+    net.add(Flatten())
     net.add(Dense(256))
     net.add(Activation('relu'))
     net.add(Dense(1))
@@ -32,13 +37,10 @@ def network(x):
 def main():
     images, angles = load_data()
 
-    print(images.shape)
-    print(images[0])
-
     net = network(images)
 
     net.compile(loss='mse', optimizer='adam')
-    net.fit(images, angles, nb_epoch=100, batch_size=128, validation_split=0.2, shuffle=True, verbose=2)
+    net.fit(images, angles, nb_epoch=10, batch_size=128, validation_split=0.2, shuffle=True, verbose=2)
     net.save('net.h5')
 
 if __name__ == '__main__':
