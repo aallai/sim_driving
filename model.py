f108f8f95352ec731e15ea613a360b227bce52dc
Alexandre Allaire <alexandre.allaire@mail.mcgill.ca>
2017-10-16 02:20:53 +0000

    Add layers, fix samples_per_epoch.

--- model.py
+++ model.py
@@ -3,14 +3,14 @@ from keras.layers import Input, Flatten, Dense, Activation, Lambda, Conv2D, MaxP
 import cv2
 import csv
 import numpy as np
-from sklearn.utils import shuffle
+import sklearn
+import math
 
 TRAIN_DATA_PATH = '/home/carnd/data'
 VALID_DATA_PATH = '/home/carnd/valid_data'
 IMAGE_SHAPE =(160, 320, 3)
 BATCH_SIZE = 64
 
-# TODO: use other features.
 class data_gen():
 
     def __init__(self, data_path, batch_size=128):
@@ -48,17 +48,17 @@ class data_gen():
            self.reset()
 
         data, labels = np.array(images, dtype=np.float32), np.array(angles, dtype=np.float32)
-        return shuffle(data, labels)
+        return sklearn.utils.shuffle(data, labels)
 
+# Have to account for augmentation here.
 def count_dataset(data_path):
     csvfile = open('/'.join([data_path, 'driving_log.csv']))
     next(csvfile)
     num = 0
     for line in csv.reader(csvfile):
         num += 1
-    return num
+    return num * 2
 
-# TODO: improve architecture.
 def network():
     net = keras.models.Sequential()
 
@@ -67,35 +67,40 @@ def network():
 
     # Normalize.
     net.add(Lambda(lambda x: (x / 255.0) - 0.5))
-    
-    # Merge color channels
-    #net.add(Conv2D(nb_filter=1, nb_row=1, nb_col=1, subsample=(1,1), border_mode='valid'))
 
     # Output: 100x300x16  
     conv1 = Conv2D(nb_filter=16, nb_row=11, nb_col=21, subsample=(1,1), border_mode='valid', bias=True)
     net.add(conv1) 
     net.add(Activation('relu'))
 
-    # Output: 25x75x16
-    net.add(MaxPooling2D(pool_size=(4,4), strides=(4,4), border_mode='valid'))
+    # Output: 50x150x16
+    net.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
 
-    # Output 20x70x32
-    conv2 = Conv2D(nb_filter=32, nb_row=6, nb_col=6, subsample=(1,1), border_mode='valid', bias=True)
+    # Output 42x142x32
+    conv2 = Conv2D(nb_filter=32, nb_row=9, nb_col=9, subsample=(1,1), border_mode='valid', bias=True)
     net.add(conv2)
+    net.add(Activation('relu'))
+
+    # Outout 21x71x32
+    net.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
+
+    # 18x68x64
+    conv3 = Conv2D(nb_filter=64, nb_row=4, nb_col=4, subsample=(1,1), border_mode='valid', bias=True)
+    net.add(conv3)
     net.add(Activation('relu'))
 
     net.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
 
     net.add(Flatten())
-    net.add(Dropout(p=0.7))
 
     net.add(Dense(256))
     net.add(Activation('relu'))
-    net.add(Dropout(p=0.7))
 
     net.add(Dense(256))
     net.add(Activation('relu'))
-    net.add(Dropout(p=0.7))
+
+    net.add(Dense(256))
+    net.add(Activation('relu'))
 
     net.add(Dense(1))
     return net
@@ -108,17 +113,25 @@ def main():
     train_data = data_gen(TRAIN_DATA_PATH, BATCH_SIZE)
     valid_data = data_gen(VALID_DATA_PATH, BATCH_SIZE)
 
-    print("Size of training data (without 2x augmentation): {}.".format(dataset_size))
+    print("Size of training data: {}.".format(dataset_size))
     print("Size of validation data: {}.".format(validation_set_size))
 
-    net.compile(loss='mse', optimizer='adam')
-    net.fit_generator(train_data, samples_per_epoch=dataset_size, nb_epoch=50, verbose=2, validation_data=valid_data, nb_val_samples=validation_set_size)
-
-    for layer in net.layers:
-        print(layer, layer.trainable)
+    net.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=0.0001))
+    net.fit_generator(train_data, samples_per_epoch=dataset_size, nb_epoch=10, verbose=2, validation_data=valid_data, nb_val_samples=validation_set_size)
 
     net.summary()
     net.save('net.h5')
+
+    # Print some predictions.
+    x, y = data_gen(TRAIN_DATA_PATH, BATCH_SIZE).__next__()
+
+    pred = net.predict(x)
+
+    print("Some predictions:")
+    print(y[:10])
+    print(pred[:10])
+
+
 
 if __name__ == '__main__':
     main()
