491c87f5766e8deb392064b12fe26b04243fa477
Alexandre Allaire <alexandre.allaire@mail.mcgill.ca>
2017-10-18 04:19:36 +0000

    Add speed as input feature.

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import keras
+from keras.models import Model
 from keras.layers import Input, Flatten, Dense, Activation, Lambda, Conv2D, MaxPooling2D, Dropout
 import cv2
 import csv
@@ -6,10 +7,10 @@ import numpy as np
 import sklearn
 import math
 
-TRAIN_DATA_PATH = '/home/carnd/data'
+TRAIN_DATA_PATH = '/home/carnd/combined_data'
 VALID_DATA_PATH = '/home/carnd/valid_data'
 IMAGE_SHAPE =(160, 320, 3)
-BATCH_SIZE = 64
+BATCH_SIZE = 128
 
 class data_gen():
 
@@ -29,26 +30,31 @@ class data_gen():
 
         images = []
         angles = []
-       
+        speeds = []
+
         try:
             while len(images) < self.batch_size:
                 line = self.reader.__next__()
                 f = line[0].split('/')[-1]
                 img = cv2.imread('/'.join([self.data_path, 'IMG', f]))
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 angle = float(line[3])
+                speed = float(line[6])
 
                 images.append(img)
                 angles.append(angle)
+                speeds.append(speed)
 
                 # Augment by flipping images.
                 images.append(np.fliplr(img))
                 angles.append(-angle)
+                speeds.append(speed)
 
         except StopIteration:
            self.reset()
 
-        data, labels = np.array(images, dtype=np.float32), np.array(angles, dtype=np.float32)
-        return sklearn.utils.shuffle(data, labels)
+        images, speeds, angles = sklearn.utils.shuffle(np.array(images, dtype=np.float32), np.array(speeds), np.array(angles))
+        return [images, speeds], angles
 
 # Have to account for augmentation here.
 def count_dataset(data_path):
@@ -60,50 +66,50 @@ def count_dataset(data_path):
     return num * 2
 
 def network():
-    net = keras.models.Sequential()
+
+    image = Input(IMAGE_SHAPE, dtype='float32', name='image_input')
+    speed = Input((1,), dtype='float32', name='speed_input')
 
     # Crop off top third of image. Result is 110x320 image.
-    net.add(Lambda(lambda x: x[:,50:,...], input_shape=IMAGE_SHAPE))
+    pre1 = Lambda(lambda x: x[:,50:,...])(image)
 
     # Normalize.
-    net.add(Lambda(lambda x: (x / 255.0) - 0.5))
+    pre2 = (Lambda(lambda x: (x / 255.0) - 0.5))(pre1)
 
     # Output: 100x300x16  
-    conv1 = Conv2D(nb_filter=16, nb_row=11, nb_col=21, subsample=(1,1), border_mode='valid', bias=True)
-    net.add(conv1) 
-    net.add(Activation('relu'))
+    conv1 = Conv2D(nb_filter=16, nb_row=11, nb_col=21, subsample=(1,1), border_mode='valid', bias=True)(pre2)
+    conv1 = Activation('relu')(conv1)
 
     # Output: 50x150x16
-    net.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
+    pool1 = MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid')(conv1)
 
     # Output 42x142x32
-    conv2 = Conv2D(nb_filter=32, nb_row=9, nb_col=9, subsample=(1,1), border_mode='valid', bias=True)
-    net.add(conv2)
-    net.add(Activation('relu'))
+    conv2 = Conv2D(nb_filter=32, nb_row=9, nb_col=9, subsample=(1,1), border_mode='valid', bias=True)(pool1)
+    conv2 = Activation('relu')(conv2)
 
     # Outout 21x71x32
-    net.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
+    pool2 = MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid')(conv2)
 
     # 18x68x64
-    conv3 = Conv2D(nb_filter=64, nb_row=4, nb_col=4, subsample=(1,1), border_mode='valid', bias=True)
-    net.add(conv3)
-    net.add(Activation('relu'))
+    conv3 = Conv2D(nb_filter=64, nb_row=4, nb_col=4, subsample=(1,1), border_mode='valid', bias=True)(pool2)
+    conv3 = Activation('relu')(conv3)
 
-    net.add(MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid'))
+    pool3 = MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid')(conv3)
 
-    net.add(Flatten())
+    flat = Flatten()(pool3)
+    cat = keras.layers.merge([flat, speed], mode='concat')
 
-    net.add(Dense(256))
-    net.add(Activation('relu'))
+    fc1 = Dense(256)(cat)
+    fc1 = Activation('relu')(fc1)
 
-    net.add(Dense(256))
-    net.add(Activation('relu'))
+    fc2 = Dense(256)(fc1)
+    fc2 = Activation('relu')(fc2)
 
-    net.add(Dense(256))
-    net.add(Activation('relu'))
+    fc3 = Dense(256)(fc2)
+    fc3 = Activation('relu')(fc3)
 
-    net.add(Dense(1))
-    return net
+    out = Dense(1)(fc3)
+    return Model(input=[image, speed], output=[out])
 
 def main():
     net = network()
@@ -117,20 +123,10 @@ def main():
     print("Size of validation data: {}.".format(validation_set_size))
 
     net.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=0.0001))
-    net.fit_generator(train_data, samples_per_epoch=dataset_size, nb_epoch=10, verbose=2, validation_data=valid_data, nb_val_samples=validation_set_size)
+    net.fit_generator(train_data, samples_per_epoch=dataset_size, nb_epoch=5, verbose=2, validation_data=valid_data, nb_val_samples=validation_set_size)
 
     net.summary()
     net.save('net.h5')
-
-    # Print some predictions.
-    x, y = data_gen(TRAIN_DATA_PATH, BATCH_SIZE).__next__()
-
-    pred = net.predict(x)
-
-    print("Some predictions:")
-    print(y[:10])
-    print(pred[:10])
-
 
 
 if __name__ == '__main__':
