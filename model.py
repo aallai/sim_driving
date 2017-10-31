987607c44fe5d56e523b5c2dba53cbfe79373e76
Alexandre Allaire <alexandre.allaire@mail.mcgill.ca>
2017-10-31 01:13:43 +0000

    Revert "Augment data using left & right cameras."
    This reverts commit 466ad4a0710598d7e9a198712ec7b288986faa70.

--- model.py
+++ model.py
@@ -8,11 +8,10 @@ import numpy as np
 import sklearn
 import math
 
-TRAIN_DATA_PATH = '/home/carnd/clean_data'
+TRAIN_DATA_PATH = '/home/carnd/data'
 VALID_DATA_PATH = '/home/carnd/valid_data'
 IMAGE_SHAPE =(160, 320, 3)
 BATCH_SIZE = 128
-OFF_CENTER_CORRECTION = 0.2 # Steering angle offset for left & right camera frames.
 
 class data_gen():
 
@@ -37,46 +36,19 @@ class data_gen():
         try:
             while len(images) < self.batch_size:
                 line = self.reader.__next__()
-                f_c = line[0].split('/')[-1]
-                f_l = line[1].split('/')[-1]
-                f_r = line[2].split('/')[-1]
-
-                img_c = cv2.imread('/'.join([self.data_path, 'IMG', f_c]))
-                img_c = cv2.cvtColor(img_c, cv2.COLOR_BGR2RGB)
-
-                img_l = cv2.imread('/'.join([self.data_path, 'IMG', f_l]))
-                img_l = cv2.cvtColor(img_l, cv2.COLOR_BGR2RGB)
-
-                img_r = cv2.imread('/'.join([self.data_path, 'IMG', f_r]))
-                img_r = cv2.cvtColor(img_r, cv2.COLOR_BGR2RGB)
-
+                f = line[0].split('/')[-1]
+                img = cv2.imread('/'.join([self.data_path, 'IMG', f]))
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 angle = float(line[3])
                 speed = float(line[6])
 
-                # Augment by adding left & right camera images with offset angle.
-                images.append(img_c)
+                images.append(img)
                 angles.append(angle)
                 speeds.append(speed)
 
-                images.append(img_l)
-                angles.append(angle + OFF_CENTER_CORRECTION)
-                speeds.append(speed)
-
-                images.append(img_r)
-                angles.append(angle - OFF_CENTER_CORRECTION)
-                speeds.append(speed)
-
                 # Augment by flipping images.
-                images.append(np.fliplr(img_c))
+                images.append(np.fliplr(img))
                 angles.append(-angle)
-                speeds.append(speed)
-
-                images.append(np.fliplr(img_l))
-                angles.append(-angle - OFF_CENTER_CORRECTION)
-                speeds.append(speed)
-
-                images.append(np.fliplr(img_r))
-                angles.append(-angle + OFF_CENTER_CORRECTION)
                 speeds.append(speed)
 
         except StopIteration:
@@ -92,7 +64,7 @@ def count_dataset(data_path):
     num = 0
     for line in csv.reader(csvfile):
         num += 1
-    return num * 6
+    return num * 2
 
 def network():
 
@@ -119,6 +91,7 @@ def network():
     # Outout 21x71x32
     pool2 = MaxPooling2D(pool_size=(2,2), strides=(2,2), border_mode='valid')(conv2)
 
+    # 18x68x64
     conv3 = Conv2D(nb_filter=64, nb_row=4, nb_col=4, subsample=(1,1), border_mode='valid', bias=True)(pool2)
     conv3 = Activation('relu')(conv3)
 
@@ -160,7 +133,7 @@ def main(pretrained):
     print("Size of validation data: {}.".format(validation_set_size))
 
     net.compile(loss='mse', optimizer=keras.optimizers.Adam(lr=0.0001))
-    net.fit_generator(train_data, samples_per_epoch=dataset_size, nb_epoch=5, verbose=2, validation_data=valid_data, nb_val_samples=validation_set_size)
+    net.fit_generator(train_data, samples_per_epoch=dataset_size, nb_epoch=20, verbose=2, validation_data=valid_data, nb_val_samples=validation_set_size)
 
     net.summary()
     print("Writing model out to 'net.h5'")
